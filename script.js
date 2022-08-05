b6da1e0658f6a497c2f78749f93e81dfb41a94a0
Aumboonaew <unaewblanca@gmail.com>
2022-08-05 23:00:27 +0700

    lap5

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ const todoCtn = document.getElementById("todo-container");
 inputAdd.onkeyup = (event) => {
   if (event.key !== "Enter") return;
 
+  if (inputAdd.value === "") {
+    alert("Todo cannot ba empty");
+  } else {
+    addTodo(inputAdd.value, false);
+  }
+    inputAdd.value = "";
+    saveTodo();
   //your code here
 };
 
@@ -22,27 +29,72 @@ function addTodo(title, completed) {
   const doneBtn = document.createElement("button");
   doneBtn.innerText = "Done";
   doneBtn.className = "btn btn-success me-2";
+  doneBtn.style.display = "none";
 
   //create delete button
   const deleteBtn = document.createElement("button");
   deleteBtn.innerText = "Delete";
   deleteBtn.className = "btn btn-danger";
+  deleteBtn.style.display = "none";
 
   //your code here
+  doneBtn.onclick = () => {
+    if (span.style.textDecoration === "line-through")
+    span.style.textDecoration = "";
+    else span.style.textDecoration = "line-through";
+    saveTodo();
+  };
   //append todo to HTML...
+  div.appendChild(span);
+  div.appendChild(doneBtn);
+  div.appendChild(deleteBtn);
+  todoCtn.prepend(div);
+
+  div.onmouseover = () => {
+    doneBtn.style.display = "none";
+    deleteBtn.style.display = "none";
+  };
+  div.onmouseover = () => {
+    doneBtn.style.display = "";
+    deleteBtn.style.display = "";
+  };
   //define buttons event...
+  doneBtn.onclick = () => {
+    if (span.style.textDecoration === "line-through") {
+      span.style.textDecoration = "";
+    } else {
+      span.style.textDecoration = "line-through";
+    }
+    saveTodo();
+  };
+  deleteBtn.onclick = () => {
+    todoCtn.removeChild(div);
+    saveTodo();
+  };
 }
 
 function saveTodo() {
   const data = [];
   for (const todoDiv of todoCtn.children) {
     //your code here
+    const todoObj = {};
+    todoObj.title = todoDiv.children[0].innerText;
+    todoObj.completed = 
+      todoDiv.children[0].style.textDecoration === "line-through";
+      data.unshitft(todoObj);
   }
   //your code here
+  const dataJSON = JSON.stringify(data);
+  localStorage.setItem("todo list", dataJSON);
 }
 
 function loadTodo() {
   //your code here
+  const dataJSON = localStorage.getItem("todo list");
+  const data = JSON.parse(dataJSON);
+  for (const todoObj of data) {
+    addTodo(todoObj.title, todoObj.completed);
+  }
 }
 
 loadTodo();
